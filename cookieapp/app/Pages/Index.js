fc5c26d4f817d054dc71a541b6335e67a95bc274
ramawm20 <ramawael54@gmail.com>
2023-10-07 15:23:02 +0000

    simple change

--- cookieapp/app/Pages/Index.js
+++ cookieapp/app/Pages/Index.js
@@ -1,7 +1,13 @@
+import Header from '@/app/Pages/Header'
+import Footer from '@/app/Pages/Footer'
+import  Main  from '@/app/Pages/Main'
+
 function Index(){
     return(
         <>
-        
+         <Header></Header>
+  <Main></Main>
+  <Footer></Footer>
 
 
         </>
